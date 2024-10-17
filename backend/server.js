1f345bdf6cf5ff93cbe90a87ccb1b311515a90af
ronnylcortez <ronnylcortez12@gmail.com>
2024-10-17 12:33:42 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,29 @@ const socioRoutes = require('./routes/socioRoutes');
 const fichaMedicaRoutes = require('./routes/fichaMedicaRoutes');
 const tiposSangreRouter = require('./routes/tiposSangreRoutes');
 const factoresActividadRouter = require('./routes/factoresActividadRoutes');
-const port = 10000;
 
-// Configura CORS
-app.use(cors());
+const port = process.env.PORT || 10000;
+
+// Middleware para permitir solicitudes CORS (permite el acceso desde el frontend en otro dominio)
+// Configurar CORS
+// Configuración de CORS
+const allowedOrigins = [
+  'http://localhost:3000', // Desarrollo en localhost
+  'https://ficha-medica-ugnu.onrender.com/' // Producción
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
 // Middleware para transformar las solicitudes HTTP con cuerpo JSON en objeto JavaScript
 app.use(express.json());
 
